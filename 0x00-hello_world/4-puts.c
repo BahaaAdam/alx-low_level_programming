758327c81945f4992071358302ae4864b7441cdf
BahaaAdam <bahaa.adam69@gmail.com>
2023-03-16 16:38:16 -0700

    4-puts.c excutable

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
- #include <stdio.h>
+#include <stdio.h>
 /**
- *  main - A program that prints a sentence using function puts
- *  Return : 0 (works) 
+ *main - this is the main function
+ *Return: always 0
  */
 int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+{	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
