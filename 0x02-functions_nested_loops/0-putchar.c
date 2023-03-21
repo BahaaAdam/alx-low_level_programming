5aafccc54b58ff8bc5e6bf76d585ea70966da45b
BahaaAdam <bahaa.adam69@gmail.com>
2023-03-21 09:40:27 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * main - main fucntion
+ *
  * description: A program that writes _putcher with a new line afterwards
  * Return : 0 (success)
  */
 
-int main(void)
+void main(void)
 {
 	printf("_putcher\n");
 return (0);
