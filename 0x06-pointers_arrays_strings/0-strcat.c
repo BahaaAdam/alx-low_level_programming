056ceccab11592df275dd493c12935a65a343840
BahaaAdam <bahaa.adam69@gmail.com>
2023-03-29 17:11:54 -0700

    all mains

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - a program that concatinates two strings
+ * Return: 0 (success)
+ */
+
+
+int main(void)
+{
+
+	char dest[];
+	char src;
+
+	char *_strcat(char *dest, char *src);
+	_putchar("%s\n", dest);
+	return (0);
+}
