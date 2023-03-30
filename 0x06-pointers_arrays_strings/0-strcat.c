1818a2936fdb30ca191ebeab2da9e7b25dc5553d
BahaaAdam <bahaa.adam69@gmail.com>
2023-03-29 17:37:09 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,19 +10,11 @@
 char *_strcat(char *dest, char *src)
 {
 
-	char s1[98] = dest;
-	char s2[] = src;
-	length 1 = strlen(s1);
-	length 1 = strlen(s2);
-
-
 	for (i = 0, i <= 4, i++)
 
 	{
 		s1[length 1 + i], s2[i];
 	}
 
-
-	_putchar("%s", s1);
 	return (0);
 }
