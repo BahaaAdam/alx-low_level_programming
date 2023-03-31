7f1f3515dcf3deee5b93cecb6f89b8c637d48d31
BahaaAdam <bahaa.adam69@gmail.com>
2023-03-31 16:39:08 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,19 +3,19 @@
  * _strcmp - compare two strings
  * @s1: string 1 value
  * @s2: string 2 value
- * Return: s1[i] - s2[i]
+ * Return:s1[i] - s2[i]
  */
-
-int _strcmp(char *s1, char *s2) 
+int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
+
+	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-	if (s1[i] != s2[i]
-	{
-		return (s1[i] > s2[i]) ? 1 : -1;
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
-	i++;
-	}
-	return (s1[i] == '\0' && s2[i] == '\0') ? 0 : ((s1[i] == '\0') ? -1 : 1);
-}
+	return (0);
