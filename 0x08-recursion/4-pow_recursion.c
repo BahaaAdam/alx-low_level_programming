8adde073d0d745a242a9abb5ee7e719221f36c66
BahaaAdam <bahaa.adam69@gmail.com>
2023-04-06 17:21:56 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - a function that gives the power of two ints
+ * @x: base int
+ * @y: power int
+ * Return: power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 1)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
