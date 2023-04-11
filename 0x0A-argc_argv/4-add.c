af1c5f8ae21bc6cbbdddce9d4c187d5d0a840ae1
BahaaAdam <bahaa.adam69@gmail.com>
2023-04-10 23:57:13 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_number - checks string for digits
+ * @str : array
+ * Return: 0 (success)
+ */
+
+int check_number(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+
+/**
+ * main - entry point
+ * @argc : arguments
+ * @argv : array
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int count, str_to_int;
+	int sum = 0;
+
+	count = 1;
+
+	while (count < argc)
+	{
+		if (check_number(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
+
