b453612e113f0929abdf4e720c1f0dd132542dd7
BahaaAdam <bahaa.adam69@gmail.com>
2023-04-10 17:54:13 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc : argumant counter
+ * @argv : array
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("my name is :%s \n", argv[0]);
+	return (0);
+}
